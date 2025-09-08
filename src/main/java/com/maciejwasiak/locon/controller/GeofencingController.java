090361c392bd2cf44d362ee208056296b3f9486f
Maciej Wasiak <maciej@thundercode.pl>
2025-09-08 16:26:46 +0200

    added docs

--- src/main/java/com/maciejwasiak/locon/controller/GeofencingController.java
+++ src/main/java/com/maciejwasiak/locon/controller/GeofencingController.java
@@ -2,6 +2,8 @@ package com.maciejwasiak.locon.controller;
 
 import com.maciejwasiak.locon.dto.GeofenceEventDto;
 import com.maciejwasiak.locon.service.GeofencingService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -15,6 +17,8 @@ import java.util.List;
 @RequestMapping("/api/geofence")
 public class GeofencingController {
 
+    private static final Logger log = LoggerFactory.getLogger(GeofencingController.class);
+
     private final GeofencingService geofencingService;
 
     public GeofencingController(GeofencingService geofencingService) {
@@ -26,7 +30,14 @@ public class GeofencingController {
         @PathVariable Long zoneId,
         @RequestParam(name = "count", required = false, defaultValue = "10") int count
     ) {
+        log.info("Simulating geofence events: zoneId={}, count={}", zoneId, count);
         List<GeofenceEventDto> events = geofencingService.generateMockEvents(zoneId, count);
+        for (GeofenceEventDto e : events) {
+            log.info("Geofence {}: zone='{}' device='{}' at [{}, {}] radius={}m time={}",
+                e.eventType(), e.zoneName(), e.deviceName(),
+                String.format("%.5f", e.latitude()), String.format("%.5f", e.longitude()),
+                e.radiusMeters(), e.occurredAt());
+        }
         return ResponseEntity.ok(events);
     }
 }
